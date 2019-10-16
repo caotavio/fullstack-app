e86bc7c417bdd5ef0cf5128e343d44eb4ec7e8cf
Otavio Araujo <caotavio@hotmail.com>
2019-10-16 20:30:21 +0100

    postgres test

--- models/index.js
+++ models/index.js
@@ -10,7 +10,8 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  // sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize("postgres://yvdxjkyjfrpmxq:f13873fd59b07f3a5b676729051d15bdc8c7df60ee5f7ef464129f80926e7403@ec2-54-228-243-29.eu-west-1.compute.amazonaws.com:5432/d7p39329ncgsns", config);
 } else {
   // sequelize = new Sequelize(config.database, config.username, config.password, config);
   sequelize = new Sequelize("postgres://postgres:2325@localhost:5432/postgres", config);
